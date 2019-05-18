c9fdc09ab6827d2a61cc9605fcefa7e698058a58
xinkai.wu@tescomm.com.cn <xinkai.wu@tescomm.com.cn>
2019-05-18 12:18:42 +0800

    2

--- 自己node练习项目/博客项目/2019复习博客/routes/articles.js
+++ 自己node练习项目/博客项目/2019复习博客/routes/articles.js
@@ -4,58 +4,87 @@ var router = express.Router();
 
 /* 获取 增加文章   http://localhost:3000/articles/add  */
 router.get('/add', function(req, res, next) {
-  res.render('articles/addHtml', {title:'增加文字'})
+    res.render('articles/addHtml', { title: '增加文字', article: {} })
 });
-// 提交 增加文章
-router.post('/add', function (req, res, next) {
-  var article = req.body
+// 提交 增加文章和修改文件
+router.post('/add', function(req, res, next) {
+    var article = req.body
+    var _id = article._id
+        //有id的是修改，没有id的是添加
+    if (_id) {
+        var set = {
+            title: article.title,
+            content: article.content,
+        }
+        console.log(_id);
+        articlesMongoose.update({ _id: _id }, { $set: set }, function(err, data) {
+            if (err) {
+                req.flash('error', "更新错误")
+                res.redirect('back')
+            } else {
+                req.flash('success', "更新成功")
+                res.redirect('/')
+            }
+        })
+    } else {
+        //获取作者，登录后可以获取到
+        var user = req.session.user;
+        article.user = user;
 
-  //获取作者，登录后可以获取到
-  var user = req.session.user;
-  article.user = user;
-
-  //插入数据库
-  articlesMongoose.create(article, function (err,data) { 
-    if (err) {
-      req.flash('error', "插入错误")
-      res.redirect('back')
-    } else { 
-      req.flash('success', "发表成功")
-      res.redirect('/')
+        //插入数据库
+        articlesMongoose.create(article, function(err, data) {
+            if (err) {
+                req.flash('error', "插入错误")
+                res.redirect('back')
+            } else {
+                req.flash('success', "发表成功")
+                res.redirect('/')
+            }
+        })
     }
-  })
 });
 
 //查看详情
 
-router.get('/detail/:_id', function (req, res, next) {
-  var ID = req.params._id
-  articlesMongoose.findById(ID, function (err,data) { 
-    if (err) {
-      req.flash('error', "插入错误")
-      res.redirect('back')
-    } else { 
-      req.flash('success', "发表成功")
-      res.render('articles/detail', {detailText:data})
-    }
-  })
+router.get('/detail/:_id', function(req, res, next) {
+    var ID = req.params._id
+    articlesMongoose.findById(ID, function(err, data) {
+        if (err) {
+            req.flash('error', "插入错误")
+            res.redirect('back')
+        } else {
+            req.flash('success', "发表成功")
+            res.render('articles/detail', { detailText: data })
+        }
+    })
 
 });
 
 // 删除 
-router.get('/delete/:_id', function (req, res) {
-  articlesMongoose.remove({_id:req.params._id}, function (err,data) { 
-    if (err) {
-      req.flash('error', "删除错误")
-      res.redirect('back')
-    } else { 
-      req.flash('success', "删除成功")
-      res.redirect('/')
-    }
-  })
+router.get('/delete/:_id', function(req, res) {
+    articlesMongoose.remove({ _id: req.params._id }, function(err, data) {
+        if (err) {
+            req.flash('error', "删除错误")
+            res.redirect('back')
+        } else {
+            req.flash('success', "删除成功")
+            res.redirect('/')
+        }
+    })
 });
 
 // 修改
-
+router.get('/update/:_id', function(req, res) {
+    var ID = req.params._id
+    articlesMongoose.findById(ID, function(err, data) {
+        if (err) {
+            req.flash('error', "跳转错误")
+            res.redirect('back')
+        } else {
+            req.flash('success', "跳转成功")
+            res.render('articles/addHtml', { article: data })
+        }
+    })
+});
 
 module.exports = router;
